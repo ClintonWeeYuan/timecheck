63ce4a69df7c351abb90b229acb3b6e77bf3df9e
Clinton Wee <clinton_wee@hotmail.com>
2021-11-29 13:37:03 +0800

    Created new .d.ts file to declare analog clock module

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,6 +32,8 @@ const Home: NextPage<Props> = (props) => {
   );
 };
 
+//Here, makes call to server to get the time, and insert that into props, which will be obtained by the context Time Provider.
+
 export async function getStaticProps() {
   const res = await fetch("http://localhost:3000/api/time", { method: "GET" });
   const time = await res.json();
