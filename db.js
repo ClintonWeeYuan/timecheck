b45b6a58580aaeed7d69cd67340f7de43dd26ddc
Clinton Wee <clinton_wee@hotmail.com>
2021-12-01 16:49:55 +0800

    Added vercel env variables to db.js

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 import { DynamoDBClient, ListTablesOutput } from "@aws-sdk/client-dynamodb";
+// import AWS from "aws-sdk";
 
 // AWS.config.update({
-//   accessKeyId: process.env.DB_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.DB_SECRET_ACCESS_KEY,
-//   region: "us-east-2",
+//   region: "eu-central-1",
 // });
 
 const db = new DynamoDBClient({
+  accessKeyId: process.env.TIMECHECK_ACCESS_KEY_ID,
+  secretAccessKey: process.env.TIMECHECK_SECRET_ACCESS_KEY,
   region: "us-east-2",
 });
 
