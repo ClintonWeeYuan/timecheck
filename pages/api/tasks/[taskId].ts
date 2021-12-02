0be74505cacc36dc0167c1608ea28a1cb691cf43
Clinton Wee <clinton_wee@hotmail.com>
2021-12-02 11:57:42 +0800

    Fixed some type problems regarding req.query in pages/api/tasks/[taskId].ts

--- pages/api/tasks/[taskId].ts
+++ pages/api/tasks/[taskId].ts
@@ -1,28 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import db from "../../../db";
 import { GetItemCommand, GetItemCommandInput } from "@aws-sdk/client-dynamodb";
+import { isAfter } from "date-fns";
 
 export default async function handleRequest(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { taskId } = req.query;
+  let taskIdString: string;
 
-  if (req.method === "GET") {
-    const params: GetItemCommandInput = {
-      TableName: "tasks",
-      Key: {
-        taskId: { S: taskId },
-      },
-      ProjectionExpression: "startTime, endTime, taskName",
-    };
+  if (typeof taskId === "string") {
+    taskIdString = taskId;
+    if (req.method === "GET") {
+      const params: GetItemCommandInput = {
+        TableName: "tasks",
+        Key: {
+          taskId: { S: taskIdString },
+        },
+        ProjectionExpression: "startTime, endTime, taskName",
+      };
 
-    try {
-      const Item = await db.send(new GetItemCommand(params));
-      res.send(Item.Item);
-    } catch (err) {
-      console.log(err);
-      res.statusCode = 500;
+      try {
+        const Item = await db.send(new GetItemCommand(params));
+        res.send(Item.Item);
+      } catch (err) {
+        console.log(err);
+        res.statusCode = 500;
+      }
     }
   }
 }
