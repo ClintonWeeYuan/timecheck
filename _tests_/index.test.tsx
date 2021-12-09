8a6976e4157a39a8048590c83882d1192b672fde
Clinton Wee <clinton_wee@hotmail.com>
2021-12-09 17:20:46 +0800

    Created tests for Put and Get request in api/events/[eventId].ts

---
+++ _tests_/index.test.tsx
@@ -0,0 +1,80 @@
+import {
+  DynamoDBClient,
+  Get,
+  GetItemCommand,
+  UpdateItemCommand,
+} from "@aws-sdk/client-dynamodb";
+import { mockClient } from "aws-sdk-client-mock";
+import db from "../db";
+
+import { createMocks } from "node-mocks-http";
+
+import handleRequest from "../pages/api/events/[eventId]";
+
+const dynamoDBMock = mockClient(db);
+describe("Updating and Receiving event data from DynamoDB", () => {
+  dynamoDBMock
+    .on(GetItemCommand, {
+      TableName: "events",
+      Key: {
+        eventId: { S: "123" },
+      },
+      ProjectionExpression: "startTime, endTime, eventName",
+    })
+    .resolves({
+      Item: {
+        eventId: { S: "123" },
+        eventName: { S: "Test Get" },
+        startTime: { N: "12345" },
+        endTime: { N: "12345" },
+      },
+    })
+    .on(UpdateItemCommand, {
+      TableName: "events",
+      Key: {
+        eventId: { S: "123" },
+      },
+      UpdateExpression: "set eventName = :n, startTime = :s, endTime = :e",
+      ExpressionAttributeValues: {
+        ":n": { S: "Test Put" },
+        ":s": { N: "12345" },
+        ":e": { N: "12345" },
+      },
+      ReturnValues: "ALL_NEW",
+    })
+    .resolves({
+      Attributes: {
+        eventId: { S: "123" },
+      },
+    });
+  test("Update Event", async () => {
+    const { req, res } = createMocks({
+      method: "PUT",
+      body: JSON.stringify({
+        eventId: "123",
+        eventName: "Test Put",
+        startTime: "12345",
+        endTime: "12345",
+      }),
+    });
+    await handleRequest(req, res);
+
+    expect(res.statusCode).toStrictEqual(200);
+    expect(res._getData()).toStrictEqual("123");
+  });
+  test("Get Event", async () => {
+    const { req, res } = createMocks({
+      method: "GET",
+      query: {
+        eventId: "123",
+      },
+    });
+    await handleRequest(req, res);
+
+    expect(res.statusCode).toStrictEqual(200);
+    expect(res._getData().eventId.S).toStrictEqual("123");
+    expect(res._getData().eventName.S).toStrictEqual("Test Get");
+    expect(res._getData().startTime.N).toStrictEqual("12345");
+    expect(res._getData().endTime.N).toStrictEqual("12345");
+  });
+});
