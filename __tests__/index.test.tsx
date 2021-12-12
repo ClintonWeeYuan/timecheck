e9280a26ebc2cc7b327e65af5b96426ab8b61668
Clinton Wee <clinton_wee@hotmail.com>
2021-12-12 21:40:34 +0800

    Put dynamoDBMocks into their appropriate test function blocks

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,53 +1,33 @@
-import {
-  DynamoDBClient,
-  Get,
-  GetItemCommand,
-  UpdateItemCommand,
-} from "@aws-sdk/client-dynamodb";
+import { GetItemCommand, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
 import { mockClient } from "aws-sdk-client-mock";
 import db from "../db";
 
 import { createMocks } from "node-mocks-http";
 
 import handleRequest from "../pages/api/events/[eventId]";
-
 const dynamoDBMock = mockClient(db);
 describe("Updating and Receiving event data from DynamoDB", () => {
-  dynamoDBMock
-    .on(GetItemCommand, {
-      TableName: "events",
-      Key: {
-        eventId: { S: "123" },
-      },
-      ProjectionExpression: "startTime, endTime, eventName",
-    })
-    .resolves({
-      Item: {
-        eventId: { S: "123" },
-        eventName: { S: "Test Get" },
-        startTime: { N: "12345" },
-        endTime: { N: "12345" },
-      },
-    })
-    .on(UpdateItemCommand, {
-      TableName: "events",
-      Key: {
-        eventId: { S: "123" },
-      },
-      UpdateExpression: "set eventName = :n, startTime = :s, endTime = :e",
-      ExpressionAttributeValues: {
-        ":n": { S: "Test Put" },
-        ":s": { N: "12345" },
-        ":e": { N: "12345" },
-      },
-      ReturnValues: "ALL_NEW",
-    })
-    .resolves({
-      Attributes: {
-        eventId: { S: "123" },
-      },
-    });
   test("Update Event", async () => {
+    dynamoDBMock
+      .on(UpdateItemCommand, {
+        TableName: "events",
+        Key: {
+          eventId: { S: "123" },
+        },
+        UpdateExpression: "set eventName = :n, startTime = :s, endTime = :e",
+        ExpressionAttributeValues: {
+          ":n": { S: "Test Put" },
+          ":s": { N: "12345" },
+          ":e": { N: "12345" },
+        },
+        ReturnValues: "ALL_NEW",
+      })
+      .resolves({
+        Attributes: {
+          eventId: { S: "123" },
+        },
+      });
+
     const { req, res } = createMocks({
       method: "PUT",
       body: JSON.stringify({
@@ -63,6 +43,22 @@ describe("Updating and Receiving event data from DynamoDB", () => {
     expect(res._getData()).toStrictEqual("123");
   });
   test("Get Event", async () => {
+    dynamoDBMock
+      .on(GetItemCommand, {
+        TableName: "events",
+        Key: {
+          eventId: { S: "123" },
+        },
+        ProjectionExpression: "startTime, endTime, eventName",
+      })
+      .resolves({
+        Item: {
+          eventId: { S: "123" },
+          eventName: { S: "Test Get" },
+          startTime: { N: "12345" },
+          endTime: { N: "12345" },
+        },
+      });
     const { req, res } = createMocks({
       method: "GET",
       query: {
