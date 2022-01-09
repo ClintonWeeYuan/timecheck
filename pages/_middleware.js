069a440f62bbf23d2d262ac369ef1fbe08bb98e1
Clinton Wee <clinton_wee@hotmail.com>
2022-01-10 02:05:43 +0800

    If root url is used without an eventId, a new event is created with a new eventId, and the page is redirected to that new page with the eventId

---
+++ pages/_middleware.js
@@ -0,0 +1,20 @@
+import { NextResponse, NextRequest } from "next/server";
+const randomWords = require("random-words");
+
+export async function middleware(req, ev) {
+  const { pathname } = req.nextUrl;
+  if (pathname == "/") {
+    const newEvent = randomWords({ exactly: 3, join: "-" });
+    const res = await fetch(`/api/events/${newEvent}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        eventId: newEvent,
+        eventName: "",
+        startTime: Date.now().toString(),
+        endTime: Date.now().toString(),
+      }),
+    });
+    return NextResponse.redirect("/" + newEvent);
+  }
+  return NextResponse.next();
+}
